08f141770d19c0eae03cda0bfe3ce0bb82b23d84
a-shmyg <45519165+a-shmyg@users.noreply.github.com>
2020-10-01 19:50:19 +0100

    defining actions interface methods

---
+++ src/testGame.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("hello world");
+
+let actionsParser = new ActionParser(["look", "stats", "inv", "where"]);
+let playerOne = new Player("ana", {health:10, armour:0}, [""], "none");
+player1.outputCharInfo();
